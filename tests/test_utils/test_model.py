306c684c0686b3d683c1bafda22298f0a6c1c6bc
Sotiris Touliopoulos <109972702+SotirisTouliopoulos@users.noreply.github.com>
2025-03-18 21:30:33 +0100

    Initial commit for co-factor specificity (#5)
    * Initial commit for co-factor specificity
    
    * Update class for cofactor specificity
    
    * Update class for CofactorSpecificity and created UnitTest

--- tests/test_utils/test_model.py
+++ tests/test_utils/test_model.py
@@ -10,6 +10,7 @@ root_dir = os.path.dirname(os.path.dirname(tests_dir))
 models_dir = os.path.join(root_dir, "ext_data/models")
 ecoli = os.path.join(models_dir, "e_coli_core.xml")
 ba = os.path.join(models_dir, "B_animalis_ModelSEED.xml")
+ec_model = cobra.io.read_sbml_model(ecoli)
 ba_model = cobra.io.read_sbml_model(ba)
 
 class TestUtils(unittest.TestCase):
@@ -32,6 +33,14 @@ class TestUtils(unittest.TestCase):
         edit_model, suggested_medium = sync_with_medium(model=edit_model, medium=new_medium)
         self.assertTrue("EX_cpd00122_e0" in edit_model.exchanges)
         self.assertTrue("EX_cpd37276_e0" not in suggested_medium)
+        
+    def test_cofactor_specificity(self):
+        
+        self.assertTrue(ec_model.reactions.FORt2.bounds == (0.0, 1000.0))
+         
+        cofactor_specificity = CofactorSpecificity(ec_model)
+        cofactor_specificity.filter_reactions()
+        self.assertTrue(ec_model.reactions.FORt2.bounds == (0, 0))
 
 if __name__ == "__main__":
     unittest.main()
